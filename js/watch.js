3654fe12eaa16142695f0fea710f341fc9bdd665
Hansin1997 <845612500@qq.com>
2017-12-21 22:30:32 +0800

    直播组接口对接

--- js/watch.js
+++ js/watch.js
@@ -6,63 +6,85 @@ function getGET() {
 	var strs = window.location.search.substr(1).split("&");
 	for(var i in strs){
 		var kv = strs[i].split('=',2);
-		if(kv != null && kv.length >= 2) {
-			if(result[kv[0]] == null){
-				result[kv[0]] = new Array();
-			}
-			result[kv[0]].push(kv[1]);
-		}
+		result[kv[0]] = kv[1];
 	}
 	return result;
 }
 
-//获取app数组
-apps = GET['app'];
-if(apps != null && apps.length > 0){
-	$('#source').attr('src','/hls/' + apps[0] + '.m3u8');
+app = GET['app'];
+group = GET['group'];
+
+if(app != null){
+	$('#source').attr('src','/hls/' + app + '.m3u8');
 	$('#source').attr('type','application/x-mpegURL');
-} else {
-	apps = new Array();
+	//<type='application/x-mpegURL'>
 }
-apps['data'] = new Array();
-//绑定onload事件
-$(window).bind("load",function(){
-	resize();
-	
-	for(var index in apps) {
-		i = index;
-		$.ajax({
-			url: "http://live.xingkong.us/?s=/index/user/app&app=" + apps[i],
-			success: function(obj){
-				var res = obj;
-				if(typeof(obj) != 'object')
-					res = JSON.parse(res);
-				else
-					res = obj;
 
-				
-				if(res.status == 200 || res.status == 403){
 
-					if(res.result.appname == apps[0]){
-						$('title').text(res.result.title + ' - ' + $('title').text());
+var ajaxObj;
+if(app != null){
+	ajaxObj = $.ajax({url:"http://live.xingkong.us/?s=/index/user/app&app=" + app,async:false,dataType: 'json'});//同步加载app
+	if(ajaxObj.status == 200){
+		var obj = ajaxObj.responseText;
+		while(typeof(obj) == 'string')
+			obj = JSON.parse(obj);
+		
+		ttt = obj;
+		if(obj.status == 200 || obj.status == 403){
 
-						//video.js初始化
-						player = videojs('my-video');
-						videojs("my-video").ready(function(){
-							player = this;
-						});
-					}
-					apps['data'][res.result.appname] = res.result;
-					var item = '<li onclick="selectStream(\'' + res.result.appname + '\');">' + res.result.title + '</li>';
-					$('#m').html($('#m').html() + item);
-					$('#m').css('top',-$('#m').height() - $('#chooser').height());
-				}
+			$('title').text(obj.result.title + ' - ' + $('title').text());
 
-			},
-			dataType: 'json'});
+
+		}
+		
 	}
 	
-})
+}
+
+if(group != null){
+	ajaxObj = $.ajax({url:"http://live.xingkong.us/?s=/index/user/group&group=" + group,async:false,dataType: 'json'});//同步加载app
+	
+	if(ajaxObj.status == 200){
+		var obj = ajaxObj.responseText;
+		while(typeof(obj) == 'string')
+			obj = JSON.parse(obj);
+
+		ttt = obj;
+		if(obj.status == 200 || obj.status == 403) {
+			
+			$('title').text(obj.title + ' - ' + $('title').text());
+
+			apps = new Array();
+			for(var i in obj.apps){
+				index = obj.apps.length - i - 1;
+				if(i == 0){
+					$('#source').attr('src','/hls/' + obj.apps[i].appname + '.m3u8');
+					$('#source').attr('type','application/x-mpegURL');
+				}
+				apps[obj.apps[index].appname] = obj.apps[index];
+				
+				var item = '<li onclick="selectStream(\'' + obj.apps[index].appname + '\');">' + obj.apps[index].title + '</li>';
+				$('#m').html($('#m').html() + item);
+				$('#m').css('top',-$('#m').height() - $('#chooser').height());
+			}
+		
+		}
+		
+	}
+	
+}else{
+	$('#chooser').css('display','none');
+}
+//绑定onload事件
+$(window).bind("load",function(){
+	
+	//video.js初始化
+	player = videojs('my-video');
+	videojs("my-video").ready(function(){
+		player = this;
+	});
+	resize();
+});
 
 //定义窗口尺寸改变相应函数
 $(window).resize(resize);
@@ -74,8 +96,8 @@ function resize(){
 	
 	$('#my-video').width(width);
 	$('#my-video').height(height);
-/* 	$('#my-video_html5_api').width(width);
-	$('#my-video_html5_api').height(height); */
+ 	$('#my-video_html5_api').width(width);
+	$('#my-video_html5_api').height(height);
 	$('.video-bar').width(width - parseInt($('.video-bar').css('padding'))*2);
 	
 	var btw = 	$('.vjs-big-play-button').width();
@@ -93,12 +115,8 @@ function getUrlParam(name) {
 }
 
 
-
-
-
-
 //初始化弹幕客户端
-client = new BucketClient("live.xingkong.us",80,"websocket/" + getUrlParam("app"));
+client = new BucketClient("live.xingkong.us",80,"websocket/" + (group || app));
 
 //弹幕客户端连接服务器
 client.connect(SocketEvent);
@@ -111,6 +129,7 @@ function SocketEvent(status,evt){
 		case 'open':
 			client.sendCommand(GetDanmuCommand());
 			client.sendCommand(GetOnlineListCommand());
+			window.setInterval("client.sendCommand(GetOnlineListCommand())",10000); //定时 10s 刷新在线人数
 			break;
 			
 		//弹幕到达时
@@ -120,7 +139,7 @@ function SocketEvent(status,evt){
 				break;
 			var date = new Date(evt['sendTime']);
 			var time = date.getFullYear() + '/' + (date.getMonth() + 1) + '/' + (date.getDate()) + ' ' +  (date.getHours()>9?date.getHours():'0' + date.getHours()) + ':' + (date.getMinutes()>9?date.getMinutes():'0' + date.getMinutes());
-			var html = '<li>'  + time + "<br>" + obj + '</li>';
+			var html = '<li><span class="msgtime">'  + time + "</span><br><span class='msgbody'>" + decodeURIComponent(obj) + '</span></li>';
 			$('#danmu').html(html + $('#danmu').html());
 			break;
 			
@@ -132,7 +151,8 @@ function SocketEvent(status,evt){
 			
 		//发生错误时
 		case 'onerr':
-			console.log(evt)
+			console.log(evt);
+			client.reconnect();
 			break;
 	}
 }
@@ -141,7 +161,8 @@ function SocketEvent(status,evt){
 function sendMessage(){
 	var str = document.getElementById("in").value;
 	if(str != null && str != ""){
-		client.sendCommand(new MessageBody(str,"","","","","","").toCommand());
+		
+		client.sendCommand(new MessageBody(encodeURIComponent(str),"","","","","","").toCommand());
 	}else{
 		alert("禁止发送空弹幕！:(");
 	}
@@ -149,3 +170,28 @@ function sendMessage(){
 	document.getElementById("in").value = "";
 }
 
+show = false;
+
+function turn() {
+	show = !show;
+	if(show){
+		$('#m').css('display','inline');
+	}else{
+		$('#m').css('display','none');
+	}
+	
+	
+	
+}
+
+function selectStream(appname){
+	console.log(appname);
+	$('#source').attr('src','/hls/' + appname + '.m3u8');
+	$('#source').attr('type','application/x-mpegURL');
+	$('#my-video_html5_api').attr('src','/hls/' + appname + '.m3u8');
+	player.load('/hls/' + appname + '.m3u8');
+	player.play();
+	show = true;
+	turn();
+	
+}
